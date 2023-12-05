ae4e3f955300d5ba5d6ba8c660f6030eb98a9f4a
Marco Heßland <marco.hessland@gmail.com>
2023-12-05 12:14:02 +0100

    Solution Day 5

---
+++ src/Day05.kt
@@ -0,0 +1,48 @@
+fun main() {
+    fun parseMaps(input: List<String>) =
+        input.drop(2).joinToString("\n").split("\n\n").map { section ->
+            section.lines().drop(1).associate { row ->
+                row.split(" ").map { it.toLong() }.let { (dest, source, length) ->
+                    source..(source + length) to dest..(dest + length)
+                }
+            }
+        }
+
+    fun part1(input: List<String>): Long {
+        val seeds = input.first().substringAfter(" ").split(" ").map { it.toLong() }
+        val maps = parseMaps(input)
+        return seeds.minOf { seed ->
+            maps.fold(seed) { loc, map ->
+                map.entries.firstOrNull { loc in it.key }?.let { (source, dest) -> loc + (dest.first - source.first) } ?: loc
+            }
+        }
+    }
+
+    fun part2(input: List<String>): Long {
+        val seeds = input.first().substringAfter(" ").split(" ").map { it.toLong() }.chunked(2).map { it.first()..<it.first() + it.last() }
+        val maps = parseMaps(input)
+
+        return seeds.flatMap { seedsRange ->
+            maps.fold(listOf(seedsRange)) { locRange, map ->
+                locRange.flatMap {loc ->
+                    map.entries.mapNotNull { (source, dest) ->
+                        (maxOf(source.first, loc.first) to minOf(source.last, loc.last)).let { (start, end) ->
+                            if (start <= end) (dest.first - source.first).let { (start + it)..(end + it) } else null
+                        }
+                    }
+                }
+            }
+        }.minOf { it.first }
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day05_test")
+    val part1 = part1(testInput)
+    check(part1 == 35L)
+
+    val input = readInput("Day05")
+    part1(input).println()
+    part2(input).println()
+}
+
+
